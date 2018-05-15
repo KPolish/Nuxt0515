6b14ec333220daf637d60efcfc6032524c3aeb4a
KPolish <com94ok@naver.com>
2018-05-15 09:39:37 +0900

    20180515_nuxt#01

---
+++ commit_push.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "20180515_nuxt#01"
+git push -u origin master
---
+++ nuxt0515/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+    <div class="content">
+    <h1 class="title">러버독</h1>
+    <h2 class="subtitle">저수지의 개들</h2>
+  </div>
+  </section>
+</template>
+
+<script>
+
+</script>
